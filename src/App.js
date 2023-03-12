9b16f76ca0b350ae4597005e81a50223b1338e9e
lyhuy <lygiahuyzz1@gmail.com>
2023-03-12 10:41:42 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import BaiTapThucHanhLayout from "./BT_Layout/Layout";
+
+const App = () => {
+  return (
+    <div>
+      <BaiTapThucHanhLayout />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/BT_Layout/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Banner from "./Banner";
+import ListItem from "./ListItem";
+import Footer from "./Footer";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <ListItem />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
